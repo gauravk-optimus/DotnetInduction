8f8db4512217ea679fe7f2c985f9db23d630cd64
gauravk-optimus <gaurav.kumar@optimusinfo.com>
2017-09-07 21:25:02 +0530

    Added the extension method practice project

---
+++ ExtMethods/ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using ClassLibExtMethod;
+
+namespace ExtensionMethod1
+{
+    public static class XX
+    {
+        public static void NewMethod(this Class1 ob)
+        {
+            Console.WriteLine("Hello I m extended method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 ob = new Class1();
+            Console.WriteLine(ob.Display());
+            Console.WriteLine(ob.Print());
+            ob.NewMethod();
+            Console.ReadKey();
+        }
+    }
+}
